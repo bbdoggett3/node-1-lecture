09ae10ea41d6bdef76292ee15297d7ce5db5f7ca
bbdoggett3 <bbdoggett3@gmail.com>
2020-05-19 12:04:43 -0600

    create methods for post,delete,put

--- server/index.js
+++ server/index.js
@@ -3,7 +3,12 @@ const app = express()
 const controller = require('./controller')
 const SERVER_PORT = 4000
 
-app.use(express.json())
+app.use(express.json())    //important to remember
+
+app.use(function(req, res, next) {
+    console.log("We got a request cap'n")
+    next()
+})
 
 //endpoints, requests you are able to get
 app.get('/api/ben', (req, res) => {       //request, response
@@ -11,7 +16,9 @@ app.get('/api/ben', (req, res) => {       //request, response
 })
 
 app.get('/api/users', controller.getAllUsers)
-
 app.get('/api/users/:user_id', controller.getUserById)
+app.post('/api/users', controller.createUser)
+app.put('/api/users/user_id', controller.editUser)
+app.delete('/api/users/:user_id', controller.deleteUser)
 
 app.listen(SERVER_PORT, () => console.log(`Server running on port ${SERVER_PORT}`))
